ad0e10a700914ee45d179fc51d29d30e4bae2477
Phoebe Ho <phoebe.ho@shoalter.com>
2023-04-20 15:02:28 +0800

    try to fix swagger "Whitelabel Error Page" error

--- demo/src/main/java/com/example/demo/SwaggerConfig.java
+++ demo/src/main/java/com/example/demo/SwaggerConfig.java
@@ -11,11 +11,14 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
 
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("/**").addResourceLocations(
+                "classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations(
+                "classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations(
+                "classpath:/META-INF/resources/webjars/");
+        super.addResourceHandlers(registry);
 
     }
 }
