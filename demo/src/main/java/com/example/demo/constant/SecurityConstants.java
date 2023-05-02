d786094a7dbd1ef9118b1fba9c6cbbc8e3e3dfa7
Phoebe Ho <phoebe.ho@shoalter.com>
2023-05-02 17:15:47 +0800

    refactor: 在application.properties設定jwt secret key
    刪除SecurityConstants中的JWT_SECRET_KEY
    在JwtUtils中以@Value取出secretKey

--- demo/src/main/java/com/example/demo/constant/SecurityConstants.java
+++ demo/src/main/java/com/example/demo/constant/SecurityConstants.java
@@ -2,16 +2,8 @@ package com.example.demo.constant;
 
 public class SecurityConstants {
 
-    public static final String AUTH_LOGIN_URL = "/api/auth/login";
+//    public static final String JWT_SECRET_KEY = "p2s5v8y/B?E(H+MbQeThVmYq3t6w9z$C&F)J@NcRfUjXnZr4u7x!A%D*G-KaPdS"; //todo property
 
-    public static final String JWT_SECRET_KEY = "p2s5v8y/B?E(H+MbQeThVmYq3t6w9z$C&F)J@NcRfUjXnZr4u7x!A%D*G-KaPdS";
-
-    public static final String TOKEN_PREFIX = "Bearer ";
-
-    // Authorization request header
-    public static final String TOKEN_HEADER = "Authorization";
-
-    // token type
     public static final String TOKEN_TYPE = "JWT";
 
     // token expireTime = 2 hours
--- demo/src/main/java/com/example/demo/util/JwtUtils.java
+++ demo/src/main/java/com/example/demo/util/JwtUtils.java
@@ -4,37 +4,38 @@ import com.example.demo.constant.SecurityConstants;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.xml.bind.DatatypeConverter;
 import java.util.Date;
 
-@Component
+
 public class JwtUtils {
 
-    private static final byte[] secretKey = DatatypeConverter.parseBase64Binary(SecurityConstants.JWT_SECRET_KEY);
+    @Value("${security.jwt.secret-key}")
+    private static String secretKey;
+
+    private static final byte[] parsedSecretKey = DatatypeConverter.parseBase64Binary(secretKey);
 
     private JwtUtils() {
     }
 
     public static String generateToken(Integer id) {
-        byte[] jwtSecretKey = DatatypeConverter.parseBase64Binary(SecurityConstants.JWT_SECRET_KEY);
 
         long expiration = SecurityConstants.EXPIRATION_TIME;
 
         return Jwts.builder()
                 .setHeaderParam("typ", SecurityConstants.TOKEN_TYPE)
-                .signWith(Keys.hmacShaKeyFor(jwtSecretKey), SignatureAlgorithm.HS256)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
                 .claim("userId", id)
                 .setExpiration(new Date(System.currentTimeMillis() + expiration * 1000))
                 .compact();
     }
 
     public static Claims getTokenBody(String token) {
-        return Jwts.parserBuilder()
-                .setSigningKey(secretKey)
-                .build()
+        return Jwts.parser()
+                .setSigningKey(parsedSecretKey)
                 .parseClaimsJws(token)
                 .getBody();
     }
