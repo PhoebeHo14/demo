356b722ddaa78045d85f4024b8b3d5111030f442
Phoebe Ho <phoebe.ho@shoalter.com>
2023-05-03 10:31:27 +0800

    style: 移除SecurityConstants

--- demo/src/main/java/com/example/demo/util/JwtUtils.java
+++ demo/src/main/java/com/example/demo/util/JwtUtils.java
@@ -1,11 +1,8 @@
 package com.example.demo.util;
 
-import com.example.demo.constant.SecurityConstants;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
 import javax.xml.bind.DatatypeConverter;
 import java.util.Date;
@@ -20,13 +17,11 @@ public class JwtUtils {
 
     public static String generateToken(Integer id, String secret) {
 
-        long expiration = SecurityConstants.EXPIRATION_TIME;
-
         return Jwts.builder()
-                .setHeaderParam("typ", SecurityConstants.TOKEN_TYPE)
+                .setHeaderParam("typ", "JWT")
                 .signWith(SignatureAlgorithm.HS256, DatatypeConverter.parseBase64Binary(secret))
                 .claim("userId", id)
-                .setExpiration(new Date(System.currentTimeMillis() + expiration * 1000))
+                .setExpiration(new Date(System.currentTimeMillis() + 60 * 60 * 2L * 1000))  //2hrs
                 .compact();
     }
 
