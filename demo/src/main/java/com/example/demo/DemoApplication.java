6c623828c5374b1a94108f31d5047a137438372c
Phoebe Ho <phoebe.ho@shoalter.com>
2023-05-11 15:39:16 +0800

    feat: 在system environment配置properties

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -8,8 +8,18 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        System.setProperty("spring.profiles.active", "dev");
+        System.setProperty("spring.data.rest.base-path", "/api");
+        System.setProperty("spring.mvc.pathmatch.matching-strategy", "ANT_PATH_MATCHER");
+        System.setProperty("spring.jpa.database-platform", "org.hibernate.dialect.MySQL8Dialect");
+        System.setProperty("spring.jpa.hibernate.ddl-auto", "none");
+        System.setProperty("spring.jpa.properties.hibernate.show_sql", "true");
+        System.setProperty("spring.jpa.properties.hibernate.format_sql", "true");
+        System.setProperty("mybatis.mapper-locations", "classpath:mappers/*.xml");
+        System.setProperty("springdoc.swagger-ui.path", "/swagger-ui.html");
+
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
 }
