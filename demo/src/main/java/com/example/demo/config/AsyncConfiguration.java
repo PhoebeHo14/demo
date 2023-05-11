64ce048ed5cf376b74cce5c3c976026c228e862a
Phoebe Ho <phoebe.ho@shoalter.com>
2023-05-11 12:00:50 +0800

    style: 移除多餘的import

--- demo/src/main/java/com/example/demo/config/AsyncConfiguration.java
+++ demo/src/main/java/com/example/demo/config/AsyncConfiguration.java
@@ -1,8 +1,10 @@
 package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+@Configuration
 public class AsyncConfiguration {
 
     @Bean
@@ -10,7 +12,6 @@ public class AsyncConfiguration {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(8);
         executor.setMaxPoolSize(8);
-        executor.initialize();
         return executor;
     }
 }
