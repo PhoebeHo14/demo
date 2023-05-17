9b91df66febdf3863bfa762e4e8ad73bbc0655fa
Phoebe Ho <phoebe.ho@shoalter.com>
2023-05-17 16:07:54 +0800

    style: 移除不需要的系統環境設定

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,13 +9,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class DemoApplication {
 
     public static void main(String[] args) {
-        System.setProperty("spring.profiles.active", "dev");
-        System.setProperty("spring.data.rest.base-path", "/api");
-        System.setProperty("spring.mvc.pathmatch.matching-strategy", "ANT_PATH_MATCHER");
-        System.setProperty("spring.jpa.database-platform", "org.hibernate.dialect.MySQL8Dialect");
-        System.setProperty("spring.jpa.hibernate.ddl-auto", "none");
-        System.setProperty("mybatis.mapper-locations", "classpath:mappers/*.xml");
-        System.setProperty("springdoc.swagger-ui.path", "/swagger-ui.html");
 
         SpringApplication.run(DemoApplication.class, args);
     }
